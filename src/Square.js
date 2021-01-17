ba4e43cf905784bc945b6feaf2bdf734a5362c2d
theflaky <rajan2499@gmail.com>
2021-01-17 21:10:09 +0530

    commit

---
+++ src/Square.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Square()
+{
+    return(
+        <div>
+            <p>It matters who you learn about money from. There is a reason 99% of the world’s population competes for 1% of the resources. And it is not the fault of the rich or the ultra-rich. Instead, it is those the 99% listen to for financial advice. </p>
+        
+        </div>    
+    )
+}
+
+export default Square;
