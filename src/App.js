2b04c119aa64d0e700b23101e8c9eb9c9137b7c2
theflaky <rajan2499@gmail.com>
2021-01-24 22:30:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import './App.css';
 import Square from "./Square.js";
 import './Square.css';
 import thestack  from './thestack';
+import Dropdown from './Dropdown';
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state =
     {
-      people : thestack.username
+      people : thestack
     }
+    this.state.people.map(r => console.log(r.username))
+    console.log(this.state.people)
   }
   
   
   render(){
-      //const whatup = this.state.people.map(function (iem){return (iem.username)})
-      const stackme = thestack.map(item => <Square key={item.id} 
+    const w = this.state.people.map(r => r.username)  
+    const stackme = thestack.map(item => <Square key={item.id} 
         username={item.username} 
         username_link = {item.username_link} 
         icon_one = {item.icon[0]}
@@ -30,15 +33,16 @@ class App extends React.Component {
         sitcoms_three={item.sitcoms[2]}
         sitcoms_four={item.sitcoms[3]}
         sitcoms_five={item.sitcoms[4]}/>)
-      return (
-        <div className="App">
-          {/* <Square id = {1}/>
+    return (
+      <div className="App">
+        <Dropdown />
+      {/* <Square id = {1}/>
           <Square />
           <Square />
           <Square />
           <Square />
           <Square /> */}
-          {/* {thestack.map(item => <Square key={item.id} 
+      {/* {thestack.map(item => <Square key={item.id} 
         username={item.username} 
         sitcoms_one={item.sitcoms[0]}
         sitcoms_two={item.sitcoms[1]}
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,19 +2,28 @@ import React from 'react';
 import thestack from './thestack';
 
 class Dropdown extends React.Component{
-    // constructor(props){
-    //     super(props);
-    //     this.state = {
-    //         theusername: thestack.username
-    //     }
+    constructor(props){
+        super(props);
+        this.state = {
+            namez: thestack
+        }
+        this.state.namez.map(r => console.log(r.username))
+    }
+
+    // Function handlechange()
+    // {
+    //     return 0
     // }
     render(){
+        const w = this.state.namez.map(r => r.username)
         return(
-            <select placeholder="Sitcoms">
-                <option value='Rajan'>Rajan</option>
-                <option value='Flaky'>Flaky</option>
-                <option value='Adorable'>Adorable</option>
-                <option value='Arey'>Arey</option>
+            
+            <select placeholder="Sitcoms" onChange={this.handlechange}>
+                <option value={w[0]}>{w[0]}</option>
+                <option value={w[1]}>{w[1]}</option>
+                <option value={w[2]}>{w[2]}</option>
+                <option value={w[3]}>{w[3]}</option>
+                <option value={w[4]}>{w[4]}</option>
             </select>
         );
         }
